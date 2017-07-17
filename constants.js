70bdbcb9b2c55e095a062f0bbed948be4d6aef11
Vishwakarma, Siyaram <Siyaram.Vishwakarma@cdk.com>
2017-07-17 17:57:56 +0530

    initial code

---
+++ constants.js
@@ -0,0 +1,7 @@
+'use strict';
+define([], function () {
+    angular.module('adp.ds.eis.constants', []).constant('EISCONSTANT', {
+        //POST_API : 'http://localhost:8080/dealerLocatorServices',
+        POST_API:'https://api.github.com/users/sonusgsits08/repos'
+  });
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+var app = angular.module("app", []);
+
+app.controller("appController", function() {
+   scope.pincode="12345";
+});
